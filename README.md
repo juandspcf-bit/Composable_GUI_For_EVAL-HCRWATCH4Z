dfb51e8f719a6d2835da4e3a76f46210cf7054b7
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2023-03-22 21:22:48 +0900

    updating the readme file and fixed an issue with compose navigation

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# Composable_GUI_For_EVAL-HCRWATCH4Z
+# Composable Android App for EVAL-HCRWATCH4Z
 ### Progress
 The bluetooth low energy scanning part is almost ready, we have used basic composable functions
 like lazy list. The most advanced composable so far is the [PullRefreshIndicator](https://developer.android.com/reference/kotlin/androidx/compose/material/pullrefresh/package-summary
 ) compose function. Because PullRefreshIndicator is experimental is likely to be changed in futures
 commits.
+
 
 
 
--- app/src/main/java/com/icxcu/adsmartbandapp/screens/BluetoothScanScreen.kt
+++ app/src/main/java/com/icxcu/adsmartbandapp/screens/BluetoothScanScreen.kt
@@ -4,6 +4,7 @@ package com.icxcu.adsmartbandapp.screens
 import android.bluetooth.le.ScanCallback
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -19,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -105,13 +107,14 @@ fun BluetoothScanScreen(
             if(statusResultState==-2 || basicBluetoothAdapters.isEmpty()){
                 ListAlbumDataEmpty()
             }else{
+                ListAlbumDataEmpty()
                 ListAlbumData(
                     basicBluetoothAdapter = basicBluetoothAdapters,
                     modifier = Modifier
                         .fillMaxSize(),
                     navLambda
                 )
-                ListAlbumDataEmpty()
+
             }
 
             PullRefreshIndicator(refreshing, state, Modifier.align(Alignment.TopCenter))
