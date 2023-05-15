90ef236ec29f0c23e1a8f29bd290b244892127f5
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2023-05-15 18:11:29 +0900

    fixing some warnings in UtilsForPlotFields.kt

--- app/src/main/java/com/icxcu/adsmartbandapp/screens/plotsFields/physicalActivity/UtilsForPlotFields.kt
+++ app/src/main/java/com/icxcu/adsmartbandapp/screens/plotsFields/physicalActivity/UtilsForPlotFields.kt
@@ -3,7 +3,7 @@ package com.icxcu.adsmartbandapp.screens.plotsFields.physicalActivity
 import android.util.Log
 import java.time.Duration
 
-fun getHours() = MutableList(48) { 0 }.mapIndexed { index, _ ->
+fun getHours() = List(MutableList(48) { 0 }.size) { index ->
     val hour = "${Duration.ofHours(0).plusMinutes(30L * index).toHours()}"
     val time = hour + if ((index + 1) % 2 == 0) {
         ":30"
@@ -13,16 +13,11 @@ fun getHours() = MutableList(48) { 0 }.mapIndexed { index, _ ->
     time
 }
 
-fun getIntervals(index: Int = 0, hoursList: List<String>) = when (index) {
-    47 -> "${hoursList[46]} - 00:00"
-    else -> "${hoursList[index]} - ${hoursList[index + 1]}"
-}
-
 fun getIntegerListFromStringMap(map: String): List<Int> {
     val newMap = map.subSequence(1, map.length - 1)
     val newMapD = newMap.split(", ")
     val stepsLists = MutableList(48) { 0 }
-    newMapD.forEachIndexed { indexO, s ->
+    newMapD.forEachIndexed { _, s ->
         val split = s.split("=")
         val index = split[0].toInt()
         val value = split[1].toInt()
@@ -36,7 +31,7 @@ fun getDoubleListFromStringMap(map: String): List<Double> {
     val newMap = map.subSequence(1, map.length - 1)
     val newMapD = newMap.split(", ")
     val stepsLists = MutableList(48) { 0.0 }
-    newMapD.forEachIndexed { indexO, s ->
+    newMapD.forEachIndexed { _, s ->
         val split = s.split("=")
         val index = split[0].toInt()
         val value = split[1].toDouble()
