a3661e778277a8838366be2f4cedd11f5f5a10bf
Juan Carlos Aguilar Lopez <juandspcf@gmail.com>
2023-05-22 22:44:57 +0900

    changing lambdas in personalDataForm.kt in order to follow best practices

--- app/src/main/java/com/icxcu/adsmartbandapp/screens/mainNavBar/settings/personaInfoScreen/personalDataForm.kt
+++ app/src/main/java/com/icxcu/adsmartbandapp/screens/mainNavBar/settings/personaInfoScreen/personalDataForm.kt
@@ -1,6 +1,11 @@
 package com.icxcu.adsmartbandapp.screens.mainNavBar.settings.personaInfoScreen
 
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.icxcu.adsmartbandapp.data.entities.PersonalInfo
 import com.icxcu.adsmartbandapp.viewModels.DataViewModel
@@ -11,25 +16,15 @@ fun PersonalDataForm(
     navLambda: () -> Unit
 ) {
 
-    val getPersonalInfoDataState = {
-        dataViewModel.personalInfoDataState
-    }
 
-    val getInvalidAlertDialogState = {
-        dataViewModel.invalidAlertDialogState
-    }
 
-    val validatePersonalInfo={
-        dataViewModel.validatePersonalInfo( getPersonalInfoDataState )
-    }
+    val getPersonalInfoDataState = remember(dataViewModel) {{dataViewModel.personalInfoDataState}}
+    val getInvalidAlertDialogState = remember(dataViewModel) {{dataViewModel.invalidAlertDialogState}}
+    val validatePersonalInfo = remember(dataViewModel, getPersonalInfoDataState) {{dataViewModel.validatePersonalInfo( getPersonalInfoDataState )}}
+    val getUpdateAlertDialogState = remember(dataViewModel) {{dataViewModel.updateAlertDialogState}}
+    val getPersonalInfoListReadFromDB = remember(dataViewModel) {{dataViewModel.personalInfoListReadFromDB}}
 
-    val getUpdateAlertDialogState = {
-        dataViewModel.updateAlertDialogState
-    }
 
-    val getPersonalInfoListReadFromDB = {
-        dataViewModel.personalInfoListReadFromDB
-    }
 
     val insertPersonalData= { personalInfo:PersonalInfo->
         personalInfo.macAddress = dataViewModel.macAddressDeviceBluetooth
@@ -51,7 +46,61 @@ fun PersonalDataForm(
         insertPersonalData
     )
 
+
+
 }
+
+class PersonalInfoDataState {
+    var name by mutableStateOf("")
+    var nameTextFieldVisibility by mutableStateOf(false)
+    var date by mutableStateOf("")
+    var dateTextFieldVisibility by mutableStateOf(false)
+    var weight by mutableStateOf("")
+    var weightTextFieldVisibility by mutableStateOf(false)
+    var height by mutableStateOf("")
+    var heightTextFieldVisibility by mutableStateOf(false)
+
+    val onTextChange = { valueFromTextField: String ->
+        name = valueFromTextField
+    }
+
+    val onNameTextFieldVisibilityChange = { visibility: Boolean ->
+        nameTextFieldVisibility = visibility
+    }
+
+    val onDateTextChange = { valueFromDateTextField: String ->
+        date = valueFromDateTextField
+    }
+
+    val onDateTextFieldVisibilityChange = { visibility: Boolean ->
+        dateTextFieldVisibility = visibility
+    }
+
+    val onWeightTextChange = { valueFromWeightTextField: String ->
+        weight = valueFromWeightTextField
+    }
+
+
+
+    val onWeightTextFieldVisibilityChange = { visibility: Boolean ->
+        weightTextFieldVisibility = visibility
+    }
+
+
+
+    val onHeightTextChange = { valueFromHeightTextField: String ->
+        height = valueFromHeightTextField
+    }
+
+
+    val onHeightTextFieldVisibilityChange = { visibility: Boolean ->
+        heightTextFieldVisibility = visibility
+    }
+
+}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
