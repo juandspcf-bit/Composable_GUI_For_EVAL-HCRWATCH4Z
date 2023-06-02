bc989714d7a73418e1b7a9c4611984a36cab7e8b
juandspcf-bit <juandspcf@gmail.com>
2023-06-03 01:02:24 +0900

    fixing the calculated dates for mock repository

--- app/src/main/java/com/icxcu/adsmartbandapp/repositories/SWRepository.kt
+++ app/src/main/java/com/icxcu/adsmartbandapp/repositories/SWRepository.kt
@@ -13,6 +13,8 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.cancel
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class SWRepository {
     private val _sharedStepsFlow = MutableSharedFlow<Values>(
@@ -28,7 +30,21 @@ class SWRepository {
 
     fun requestSmartWatchData(name: String = "", macAddress: String = "") {
         Log.d("DATAX", "requestSmartWatchDataRep: ")
+
+
         jobSW = CoroutineScope(Dispatchers.Default).launch {
+            val myFormatObj:DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+            val todayLocalDateTime:LocalDateTime = LocalDateTime.now()
+            var todayFormattedDate:String = todayLocalDateTime.format(myFormatObj)
+            val yesterdayLocalDateTime:LocalDateTime = todayLocalDateTime.minusDays(1)
+            var yesterdayFormattedDate:String = yesterdayLocalDateTime.format(myFormatObj)
+            val pastYesterdayLocalDateTime:LocalDateTime = todayLocalDateTime.minusDays(2)
+            var pastYesterdayFormattedDate:String = pastYesterdayLocalDateTime.format(myFormatObj)
+
+
+
+
+
             _sharedStepsFlow.emit(Values(
                 MutableList(48) { 0 }.toList(),
                 MutableList(48) { 0.0 }.toList(),
@@ -49,9 +65,29 @@ class SWRepository {
                 MockData.valuesYesterday.date
             ))
             delay(5000)
-            _sharedStepsFlow.emit(MockData.valuesToday)
+
+            val todayValues = Values(
+                MockData.valuesToday.stepList,
+                MockData.valuesToday.distanceList,
+                MockData.valuesToday.caloriesList,
+                MockData.valuesToday.heartRateList,
+                MockData.valuesToday.diastolicList,
+                MockData.valuesToday.systolicList,
+                todayFormattedDate,
+            )
+            _sharedStepsFlow.emit(todayValues)
+
             delay(5000)
-            _sharedStepsFlow.emit(MockData.valuesYesterday)
+            val yesterdayValues = Values(
+                MockData.valuesYesterday.stepList,
+                MockData.valuesYesterday.distanceList,
+                MockData.valuesYesterday.caloriesList,
+                MockData.valuesYesterday.heartRateList,
+                MockData.valuesYesterday.diastolicList,
+                MockData.valuesYesterday.systolicList,
+                yesterdayFormattedDate,
+            )
+            _sharedStepsFlow.emit(yesterdayValues)
             Log.d("DATAX", "requestSmartWatchDataRep:  CoroutineScope(Dispatchers.Default) END")
             jobSW.cancel()
         }
